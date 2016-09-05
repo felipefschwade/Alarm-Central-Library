fcbdb6d2fa4649ddce6caafec2d5930490c02946
Felipe F. Schwade <felipefaeschwade@gmail.com>
2016-09-05 14:41:08 -0400

    Updating the example code

--- examples/AlarmCentralExample/AlarmCentralExample.ino
+++ examples/AlarmCentralExample/AlarmCentralExample.ino
@@ -1,6 +1,8 @@
 /*
  * SD.h and SPI.h for the SDcard
  * SD card attached to SPI bus in an Arduino UNO R3 as follows:
+ * If you are using another Arduino, please switch the constants at
+ * AlarmaCentral.h
  ** MOSI - pin 11
  ** MISO - pin 12
  ** CLK - pin 13
@@ -21,11 +23,10 @@ AlarmCentral myCentral = AlarmCentral(mySwitch);
 
 //Pins that will be used in the example
 #define SENSOR_PIR1 14 // Analog 0
-#define NEW_CONTROL_BUTTON 5 
+#define NEW_CONTROL_BUTTON 5 // Digital 5
 #define SIREN 15 // Analog 1
 #define RED_LED 16 // Analog 2
 #define GREEN_LED 17 // Analog 3
-//Create a int array with all the sensor pins
 void setup() {
   Serial.begin(9600);
   //For the led Pins, the first parameter is the greenLed, and the second parameter is the redLed (You might use another leds as you want)
