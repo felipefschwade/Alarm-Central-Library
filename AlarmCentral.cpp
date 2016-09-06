84f52780fcc5c8a9b3b41c10f13f09bee1a41b39
Felipe F. Schwade <felipefaeschwade@gmail.com>
2016-09-06 14:18:44 -0400

    Removing old comments

--- AlarmCentral.cpp
+++ AlarmCentral.cpp
@@ -153,7 +153,6 @@ void AlarmCentral::treatReceivedSignal(int receivedSignal) {
               }
            ledBlink(_redLed, 200);
       break;
-      //Reset your arduino after adding a new control.
       case NEW_CONTROL_ADDING:
         addNewControl(receivedSignal);
         break;
